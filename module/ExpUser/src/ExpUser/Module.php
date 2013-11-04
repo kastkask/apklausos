58aa76aa9336ec780373245ef17511f6a6c9e053
Vytautas Stankus <svycka@gmail.com>
2013-11-04 23:38:02 +0200

    Assign default role after new user registration

--- module/ExpUser/src/ExpUser/Module.php
+++ module/ExpUser/src/ExpUser/Module.php
@@ -1,7 +1,20 @@
 <?php
 namespace ExpUser;
 
+use Zend\Mvc\MvcEvent;
+
 class Module {
+    public function onBootstrap(MvcEvent $mvcEvent)
+    {
+        $zfcServiceEvents = $mvcEvent->getApplication()->getServiceManager()->get('zfcuser_user_service')->getEventManager();
+        $zfcServiceEvents->attach('register', function($e) use($mvcEvent) {
+            $user = $e->getParam('user');
+            $em = $mvcEvent->getApplication()->getServiceManager()->get('Doctrine\ORM\EntityManager');
+            $defaultUserRole = $em->getRepository('ExpUser\Entity\Role')->findOneBy(array('roleId' => 'user'));
+            $user->addRole($defaultUserRole);
+        });
+    }
+
     public function getConfig()
     {
         return include __DIR__ . '/../../config/module.config.php';
